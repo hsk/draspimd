2cf6cfc45d2958e787c8dd57a6e8e9d51c421229
hsk <sakurai777@gmail.com>
2020-11-03 01:55:08 +0900

    refactoring zvgm02

--- example/sounddriver/zvgm02/res/res.h
+++ example/sounddriver/zvgm02/res/res.h
@@ -1,7 +1,6 @@
 #ifndef _RES_RES_H_
 #define _RES_RES_H_
 
-extern const u8 cdefg[674];
 extern const u8 af03vgm[16336];
 
 #endif // _RES_RES_H_
--- example/sounddriver/zvgm02/src/vgm.h
+++ example/sounddriver/zvgm02/src/vgm.h
@@ -1,6 +1,6 @@
 #ifndef _VGM_H_
 #define _VGM_H_
 
-extern const u8 vgm[0xF1];
+extern const u8 vgm[0xF4];
 
 #endif // _VGM_H_
